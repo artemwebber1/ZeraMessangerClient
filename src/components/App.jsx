f411f0f94a840096ca52a17facfb59fd4b8c3682
Artem Chekhonin <chekhonin.artem@gmail.com>
2024-05-14 19:13:32 +0300

    Исправлен баг с отступами вниз на странице чата

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,25 @@ import { BrowserRouter } from 'react-router-dom';
 import { Header } from './Header';
 import { Content } from './Content/Content';
 import { Authentification } from './Authentification/Authentification'
+import { useState } from 'react';
 
 
 const App = () => {
     // Проверка, аутентифицирован ли пользователь
-    let isAuthentificated = localStorage.getItem("jwt") != null;
+    const isAuthentificated = localStorage.getItem("jwt") != null;
+    const [mainStyles, setMainStyles] = useState({
+        backgroundColor: "#A3E998",
+        paddingTop: "60px",
+        paddingBottom: "80%"});
 
     return (
         <div className="app">
             <BrowserRouter>
                 <Header />
-                <div className="main">
+                <div className="main" style={mainStyles}>
                     {/* Если пользователь аутентифицирован, показываем ему контент для аутентифицированных пользователей. 
                         Если нет - окно для аутентификации. */}
-                    { isAuthentificated ? <Content /> : <Authentification />}
+                    { isAuthentificated ? <Content setMainStyles={setMainStyles} /> : <Authentification />}
                 </div>
             </BrowserRouter>
         </div>
